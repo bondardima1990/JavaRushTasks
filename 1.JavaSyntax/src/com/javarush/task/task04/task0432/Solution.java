79d7c45c1825586c03531188d6bdc6aa0015c06d
dima <bondardima1990@gmail.com>
2017-07-17 21:43:16 +0300

    JavaRushTasks

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0432/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task04.task0432;
+
+
+
+/* 
+Хорошего много не бывает
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        int n = Integer.parseInt(bufferedReader.readLine());
+        while (n > 0){
+            System.out.println(s);
+            n--;
+        }
+    }
+}
